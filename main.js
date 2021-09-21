3e7012eff2b3233f285e0ab446cbee90bc22b193
Kamalesh <35285397+kamalesh777@users.noreply.github.com>
2021-09-21 17:25:58 +0530

    done

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
 const path = require("path");
 const fs = require("fs");
 
-const directoryPath = path.join(__dirname, "folder");
+const directoryPath = path.join(__dirname, "js");
 
-fs.readdir(path.resolve(__dirname, "folder"), (err, files) => {
+fs.readdir(path.resolve(__dirname, "js"), (err, files) => {
   if (err) throw err;
 
-  files.forEach(function (file, index) {
 
-    file = file.substring(0, file.length - 6);
+  files.sort(function(x,y){
+    var xp = x.substr(-6, 4);
+    var yp = y.substr(-6, 4);
+    return xp == yp ? 0 : xp < yp ? -1 : 1;
+  });
+  // console.log(files);
+
+  files.forEach(function (file, index) {
+    
+    file = file.substring(0, file.length - 7);
 
     fs.rename(
-      `./folder/${file}#${index + 1}.txt`,
-      `./folder/#${index + 1}read.txt`,
+      `./js/${file}#${index + 1}.mp4`,
+      `./js/${index + 1 +" "+ file}.mp4`,
       (err) => {
         if (err) throw err;
         // console.log("rename complete");
